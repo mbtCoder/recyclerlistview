7bb6e3eae78f609db62c28334a593b294deaeef1
bryant <bttt@me.com>
2020-05-29 20:04:11 +0800

    同步类型确实

--- src/core/scrollcomponent/BaseScrollComponent.tsx
+++ src/core/scrollcomponent/BaseScrollComponent.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
-import { Dimension } from "../dependencies/LayoutProvider";
-import BaseScrollView, { ScrollEvent, ScrollViewDefaultProps } from "./BaseScrollView";
+import {Dimension} from "../dependencies/LayoutProvider";
+import BaseScrollView, {ScrollEvent, ScrollViewDefaultProps} from "./BaseScrollView";
+import {NativeScrollEvent, NativeSyntheticEvent, ScrollViewProps, StyleProp, ViewStyle} from "react-native";
 
-export interface ScrollComponentProps {
+export interface ScrollComponentProps extends CustomRefreshProps {
     onSizeChanged: (dimensions: Dimension) => void;
     onScroll: (offsetX: number, offsetY: number, rawEvent: ScrollEvent) => void;
     contentHeight: number;
@@ -17,6 +18,47 @@ export interface ScrollComponentProps {
     renderContentContainer?: (props?: object, children?: React.ReactNode) => React.ReactNode | null;
     renderAheadOffset: number;
 }
-export default abstract class BaseScrollComponent extends React.Component<ScrollComponentProps, {}> {
+
+/**
+ *  下拉刷新&上拉加载 类型补充
+ */
+interface CustomRefreshProps {
+    scrollViewProps: ScrollViewProps; // 库源类型补充
+    onEndReached?: ((info: { distanceFromEnd: number }) => void) | null; // 库源类型补充
+    refreshText?: string;
+    refreshingText?: string;
+    endingText?: string;
+    endText?: string;
+    noDataText?: string;
+    refreshedText?: string;
+    refreshType?: string;
+    onRefresh?: () => void;
+    flag?: string;
+    onScrollBeginDrag?: (event?: NativeSyntheticEvent<NativeScrollEvent>) => void;
+    indicatorArrowImg?: {
+        style: StyleProp<ViewStyle>
+        url: string;
+    };
+    arrowStyle?: StyleProp<ViewStyle>;
+    indicatorImg?: {
+        style: StyleProp<ViewStyle>;
+        url: string;
+    };
+}
+
+/**
+ *  下拉刷新&上拉加载 类型补充
+ */
+interface ScrollComponentState {
+    loadTitle: string;
+    prTitle: string;
+    prLoading: boolean;
+    prArrowDeg: any;
+    prTimeDisplay: string;
+    beginScroll: boolean;
+    prState: number;
+}
+
+export default abstract class BaseScrollComponent extends React.Component<ScrollComponentProps, ScrollComponentState> {
     public abstract scrollTo(x: number, y: number, animate: boolean): void;
 }
